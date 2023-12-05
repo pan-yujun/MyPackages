15d2002cfe826b791af6ee831cccbe499d20e4a7
panyujun <718728965@qq.com>
2023-12-06 01:21:44 +0800

    fix:释放资源

--- Assets/Scripts/UIOperate.cs
+++ Assets/Scripts/UIOperate.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AddressableAssets;
-
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class UIOperate : MonoBehaviour
 {
     public Button btn;
-    
+    public Button btn1;
+    public Button btn2;
+    public RawImage rawImage;
+
+
+    private List<GameObject> gos = new List<GameObject>();
+    private AsyncOperationHandle<GameObject> btn1Handle;
+    private Texture2D tx2D;
     // Start is called before the first frame update
     void Start()
     {
         btn.onClick.AddListener(OnBtnClick);
+        btn1.onClick.AddListener(OnBtn1Click);
+        btn2.onClick.AddListener(OnBtn2Click);
     }
 
     // Update is called once per frame
@@ -24,9 +33,15 @@ public class UIOperate : MonoBehaviour
     private void OnDestroy()
     {
         btn.onClick.RemoveListener(OnBtnClick);
+        btn1.onClick.RemoveListener(OnBtn1Click);
+        btn2.onClick.RemoveListener(OnBtn2Click);
     }
 
     int count = 0;
+
+    /// <summary>
+    /// 创建物体
+    /// </summary>
     public void OnBtnClick()
     {
         count++;
@@ -40,7 +55,45 @@ public class UIOperate : MonoBehaviour
             GameObject prefabObj = handle.Result;
             // 实例化
             GameObject wamon = Instantiate(prefabObj,null);
+            wamon.name = string.Format("remoteOb_{0}",count);
+            gos.Add(wamon);
             wamon.transform.localPosition = new Vector3(-(1+ count),0,0);
         };
+
+        // 加载图片
+        Addressables.LoadAssetAsync<Texture2D>("Assets/Testures/img_Lrc_ShapeFX.png").Completed += (obj) =>
+        {
+            // 预设物体
+            tx2D = obj.Result;
+            rawImage.texture = tx2D; 
+        };
+    }
+    /// <summary>
+    /// 删除物体
+    /// </summary>
+    public void OnBtn2Click()
+    {
+        foreach (var go in gos)
+        {
+            Debug.Log(string.Format("删除物体:{0}",go.name));
+            Destroy(go);
+        }
+        if (rawImage != null)
+        {
+            Debug.Log(string.Format("删除物体:{0}", rawImage.gameObject.name));
+            Destroy(rawImage.gameObject);
+        }
+    }
+    /// <summary>
+    /// 释放资源
+    /// </summary>
+    public void OnBtn1Click()
+    {
+        // 释放资源
+        if (tx2D != null)
+        { 
+            Debug.Log("释放资源");
+            Addressables.Release(tx2D);
+        }
     }
 }
